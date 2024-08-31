b3ff4e0c258a48ab587c2a4fba1e1410f5752f34
PanizHeidari <armotisarmotis@gmail.com>
2024-09-01 00:34:36 +0330

    backend is getting better :)

--- Flaskbackend.py
+++ Flaskbackend.py
@@ -1,5 +1,5 @@
 # importing required modules
-from flask import Flask, render_template 
+from flask import Flask, render_template, redirect , session , request
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random 
 from string import ascii_uppercase 
@@ -17,6 +17,19 @@ socketio= SocketIO(app)
 @app.route("/",methods=["POST","GET"])
 
 def home():
+    if request.method == "POST":
+        name=request.form.get("name")
+        code=request.form.get("code")
+        join=request.form.get("join", False)
+        create=request.form.get("create", False)
+
+        if not name:
+             return render_template("home.html", error="Please Enter a Name.")
+
+
+
+
+
     return render_template("home.html")
 
 
