66d17a4071ab5313ff3a4a322705ba74f4af72c8
PanizHeidari <armotisarmotis@gmail.com>
2024-09-07 19:53:53 +0330

    chat box was created

--- Flaskbackend.py
+++ Flaskbackend.py
@@ -85,6 +85,9 @@ def home():
 @app.route("/room")
 
 def room():
+    room=session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return(redirect(url_for("home")))
     return render_template("room.html")
 
 
