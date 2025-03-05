649c81c502da3062dd0e5155e1a2af1d96474fad
PanizHeidari <armotisarmotis@gmail.com>
2025-03-05 23:07:22 +0330

    backgroun not workign for phone

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,10 @@
+
 export default function Home() {
   return (
-    <div
-      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
-      style={{
-        backgroundImage: "url('/background-create-page.png')", // Path to the image in public folder
-        backgroundSize: 'cover', // Make sure it covers the whole screen
-        backgroundPosition: 'center', // Position the image in the center
-      }}
-    >
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         here should come the content
       </main>
     </div>
   );
 }
-
